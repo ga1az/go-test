aad7a33b2957cdbafb94937d0b7af8c591ffbc1b
ga1az <75758899+ga1az@users.noreply.github.com>
2023-01-08 22:30:48 +0000

    feat suma

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ func saludo() string{
 	return "Hola como estas"
 }
 
+func suma(n int, x int) (int){
+	return x + n
+}
+
 func main(){
 	fmt.Println("Hola como estas")
 	fmt.Println(saludo())
+	fmt.Println(suma(1,2))
 }
